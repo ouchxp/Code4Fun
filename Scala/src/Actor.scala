add78d27141c33e3e06bbf13f7274c23750ceb38
Nan Wu <ouchxp.me@gmail.com>
2014-12-10 13:58:28 +1300

    Simplify code

--- Scala/src/Actor.scala
+++ Scala/src/Actor.scala
@@ -8,33 +8,33 @@ object TestActor extends App {
   class Receiver extends Actor {
     def receive = {
       case ObjectMessage(who) => println("Hello " + who)
-      case "kill" => println("killing..."); context.system.shutdown; println("killed!");
+      case "kill" => println("killing..."); context.system.shutdown; println("killed!")
       case i: Int => Thread.sleep(i)
       case x: String => println(x)
     }
   }
-
-  val system = ActorSystem.create("MySystem")
+  
+  val system = ActorSystem create "MySystem"
   val greeter = system.actorOf(Props[Receiver], name = "greeter")
-  val GREETER_PATH = greeter.path.toString()
+  val GREETER_PATH = greeter.path.toString
 
   def sayHello = {
     val t = new Thread(new Runnable() {
       def run() {
-        Thread.sleep(4000);
-        val as = system.actorSelection(GREETER_PATH);
+        Thread sleep 4000
+        val as = system actorSelection GREETER_PATH
         as ! 3000
-        println("please say it");
+        println("please say it")
         as ! "Hello"
-        Thread.sleep(1000)
+        Thread sleep 1000
         as ! "kill"
       }
-    });
-    t.start();
+    })
+    t.start
   }
 
   sayHello
-  system.actorSelection(GREETER_PATH) ! ObjectMessage("Tom")
-  println("main finished");
+  (system actorSelection GREETER_PATH) ! ObjectMessage("Tom")
+  println("main finished")
 }
 
