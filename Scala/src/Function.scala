fd228d583b505477f7744bcbc64e59818b0a6ed8
Nan Wu <ouchxp.me@gmail.com>
2014-12-10 15:19:47 +1300

    Add function for fun

---
+++ Scala/src/Function.scala
@@ -0,0 +1,43 @@
+object TestFunctions extends App {
+
+  // Function
+  def f1(x: String) = x match {
+    case "Good" => println("Feels Good!")
+    case _ => println("No good")
+  }
+
+  val f2 = (x: String) => x match {
+    case "Good" => println("Feels Good!")
+    case _ => println("No good")
+  }
+
+  //use function can omit the match keyword
+  val f3: Function[String, Unit] = {
+    case "Good" => println("Feels Good!")
+    case _ => println("No good")
+  }
+
+  val pf1: PartialFunction[String, Unit] = {
+    case "Good" => println("Feels Good!")
+    case _ => println("No good")
+  }
+  //----------------------------------------------
+
+  val f4: Function[Object, String] = {
+    case "Good" => println("Feels Good!"); "done";
+    case _ => println("No good"); "done";
+  }
+
+  def higherOrderFun(f: Function[String, Object]) {
+    f.apply("Good");
+  }
+
+  /**
+   * higherOrderFun can accept Function[Object, String]
+   * it means Function[Object, String] <(is subclass of) Function[String, Object]
+   * So in function, argument type is contravariant, and return type is covariant 
+   * The definition of Function class should be Function[-A, +B]
+   */
+  higherOrderFun(f4)
+
+}
--- Scala/src/Trait.scala
+++ Scala/src/Trait.scala
@@ -1,43 +1,45 @@
-trait A {
-  def m1 = println("m1 called")
-}
-
-trait B {
-  def m2 = println("m2 from B called")
-}
-
-trait C extends A {
-  def m2 = println("m2 from C called")
-}
-
-// Solve mixin methods conflict
-abstract class D(i: Int) extends B with C {
-  override def m2 = {
-    super[B].m2 // call m2 in B
-    super[C].m2 // call m2 in B
+object TestTrait extends App {
+  trait A {
+    def m1 = println("m1 called")
   }
 
-  def m3 = println("m3 called, i = " + i)
-}
+  trait B {
+    def m2 = println("m2 from B called")
+  }
 
-class E(i: Int) extends D(i) {
-  def m4 = println("m4 called");
-}
+  trait C extends A {
+    def m2 = println("m2 from C called")
+  }
 
-/**
- *  A trait extends a class
- *  this only means all classes that mix-in F trait must extend E class.
- *  @see http://stackoverflow.com/questions/12854941/why-can-a-scala-trait-extend-a-class
- */
-trait F extends E {}
+  // Solve mixin methods conflict
+  abstract class D(i: Int) extends B with C {
+    override def m2 = {
+      super[B].m2 // call m2 in B
+      super[C].m2 // call m2 in B
+    }
 
-class G(i: Int) extends E(i) with F {}
-// Error
-// class G(i: Int) with F {}
+    def m3 = println("m3 called, i = " + i)
+  }
 
-class H {}
+  class E(i: Int) extends D(i) {
+    def m4 = println("m4 called");
+  }
 
-object E extends App {
+  /**
+   *  A trait extends a class
+   *  this only means all classes that mix-in F trait must extend E class.
+   *  @see http://stackoverflow.com/questions/12854941/why-can-a-scala-trait-extend-a-class
+   */
+  trait F extends E {}
+
+  class G(i: Int) extends E(i) with F {}
+  // Error
+  // class G(i: Int) with F {}
+
+  class H {}
+
+  class I extends H {}
+
   val e = new E(1)
   e.m1
   e.m2
@@ -48,19 +50,24 @@ object E extends App {
     case x: C => x.m1
     case _ => println("No m1 found")
   }
-  
+
   val h = new H with C
   // Error
   // val h = new H with F
   h.m1
   h.m2
-  
+
   val hWithoutMixin = new H
-  
+
   callM1(hWithoutMixin)
   callM1(h)
 
+  val i = new I with C
+  callM1(i)
+
 }
+
+
 
 // Need have a look this 
 // http://stackoverflow.com/questions/10291176/how-to-use-scala-trait-with-self-reference
