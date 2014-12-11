e9021ddcf5f8fcca29827d618788a2ec03fd2ab7
Nan Wu <ouchxp.me@gmail.com>
2014-12-11 17:03:35 +1300

    aa

--- Scala/src/Macros.scala
+++ Scala/src/Macros.scala
@@ -1,7 +1,19 @@
+import language.experimental.macros
+import scala.reflect.macros.whitebox.Context
+object Macros {
+  def impl(c: Context) = c.universe.reify(println("hello world!"))
+  def hello: Unit = macro impl
+}
+
+
 object TestMacros extends App {
   /**
    * @see http://docs.scala-lang.org/overviews/macros/overview.html
    */
-  
+
   //TODO: check out macros
+
+  import Macros._
+  // cannot use this check out why
+  //hello  
 }
