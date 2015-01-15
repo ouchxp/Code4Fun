2829d736a2fd8daefa39b5dd5b8c1db207ec85d8
Nan Wu <ouchxp.me@gmail.com>
2015-01-15 17:43:24 +1300

    add retry implementations

--- Scala/src/Concurrent.scala
+++ Scala/src/Concurrent.scala
@@ -19,9 +19,13 @@ object Concurrent extends App {
 
   def anotherExpensiveComputation(x: String) = {
     Thread.sleep(1000)
+    //throw new Exception("afff")
     x + "abc"
   }
 
+  // Futures are started as soon as they are declared.
+  // If don't want started should use def instead of val, because it is lazy
+  // (but what's the lazy keyword mean??? I forgot....)
   val f1 = Future {
     //if (true) throw new Exception("exp");
     expensiveComputation
@@ -32,9 +36,17 @@ object Concurrent extends App {
     case Failure(t) => println("f1: failed with exception " + t)
     case Success(v) => println("f2: " + v)
   }
-  
-  val f3 = f2.recover { case t => t.getMessage }
-  
+
+  val f3 = f2.recoverWith {
+    case t => {
+      println("f3 recover")
+      Future {
+        println("f3-f2 recoverWith");
+        t.getMessage
+      }
+    }
+  }
+
   f2.onComplete {
     case Failure(t) => println("f2: failed with exception " + t)
     case Success(v) => println("f2: " + v)
@@ -45,9 +57,76 @@ object Concurrent extends App {
     case Success(v) => println("f3: " + v)
   }
 
-  // Make sure program does not terminate (if program terminate, then the onComplete method will not run)
-  
+  f1.fallbackTo {
+    Future {
+      println("executing fallbackto")
+      "fallback"
+    }
+  }
+
+  def badretry(times: Int)(block: Future[String]): Future[String] = {
+    if (times == 0) {
+      Future.failed(new Exception("Sorry"))
+    } else {
+      block fallbackTo {
+        badretry(times - 1) { block }
+      }
+    }
+  }
+
+  // fallbackto method is not lazy evaluate, so the two futures will run in parallel
+  // Not one after another failed
+  badretry(10) {
+    Future {
+      println("badretry trying")
+      anotherExpensiveComputation("fffr")
+    }
+  }.onComplete {
+    case Failure(t) => println("badretry f1: failed with exception " + t)
+    case Success(v) => println("badretry f1: " + v)
+  }
+
+  //------------------------
+  def retryUsingRecover(times: Int)(block: Future[String]): Future[String] = {
+    val attempts = (1 to times).map(_ => () => block)
+    val failed = Future.failed[String](new Exception)
+    // recover with is call by name, lazy evaluate block
+    // the case part is an partial function, and it's call by name. 
+    // so this function will only be called when using it.
+    attempts.foldLeft(failed)((a, blockFunc) => a recoverWith { case _ => blockFunc() })
+  }
+
+  retryUsingRecover(10) {
+    Future {
+      println("retryUsingRecover trying")
+      anotherExpensiveComputation("resultstr")
+    }
+  }.onComplete {
+    case Failure(t) => println("retryUsingRecover f1: failed with exception " + t)
+    case Success(v) => println("retryUsingRecover f1: " + v)
+  }
+  //--------------------------
+  //TODO: need work on it why.....
+  def retryUsingFallbackto(noTimes: Int)(block: => Future[String]): Future[String] = {
+    val attempts = (1 to noTimes).map(_ => () => block)
+    val failed = Future.failed[String](new Exception)
+    val fold = attempts.foldRight(() => failed)(_)
+    val resultFunc = fold((block, a) => () => { block() fallbackTo { a() } })
+    resultFunc()
+  }
+
+  retryUsingFallbackto(10) {
+    Future {
+      println("retryUsingFallbackto trying")
+      anotherExpensiveComputation("resultstr")
+    }
+  }.onComplete {
+    case Failure(t) => println("retryUsingFallbackto f1: failed with exception " + t)
+    case Success(v) => println("retryUsingFallbackto f1: " + v)
+  }
+
+  // Wait for result
   val c = Await.result(f3, 2 seconds)
   println(c)
-  //Thread.sleep(100000);
+  Thread.sleep(100000);
 }
