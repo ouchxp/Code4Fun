0ac7a379e9f187630960dff939cd744e5f4237bf
Nan Wu <ouchxp.me@gmail.com>
2015-01-19 16:12:02 +1300

    update Lazy evaluations

--- Scala/src/LazyEval.scala
+++ Scala/src/LazyEval.scala
@@ -1,3 +1,11 @@
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.util.Failure
+import scala.util.Success
+import scala.util.Try
+import scala.concurrent.Await
+import scala.concurrent.Promise
 object LazyEval extends App {
 
   def defstr = { // actually is a function
@@ -37,4 +45,18 @@ object LazyEval extends App {
 
   f1lazy(lazystr)
 
+  def f2lazy(arg: => Future[String]) = {
+    /** This will evaluate args 3 times immediately */
+    //List(1,2,3).map { x => () => arg }
+
+    /** This will not evaluate any future. since all args were wrapped as function. */
+    List(1, 2, 3).map { x => () => arg }
+
+    /** Stream only evaluate the first one(automatically wrap the remainders to functions (thunk) ) */
+    //Stream(1,2,3).map { x => arg }
+  }
+
+  // This will evaluate future with call-by-name since it is a expression not value, it will not print "run" immediately
+  val func = f2lazy(Future { println("run"); "abc" })
+  Thread.sleep(1000);
 }
