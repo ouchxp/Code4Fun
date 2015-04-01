3d42013c397e94387906d793232ef81ea734b942
ouchxp <ouchxp.me@gmail.com>
2015-04-02 00:45:51 +1300

    add ex1.3

--- SICP/ex1.rkt
+++ SICP/ex1.rkt
@@ -1,1 +1,10 @@
+#lang racket
 
+; 1.3 wrong translation should be sum of square
+(define (sum-max2 x y z)
+  (+ x y z (- (min x y z))))
+
+(define (sum-of-squares x y z)
+  (let ([m (max x y)]
+        [n (max (min x y) z)])
+    (+ (* n n) (* m m))))
