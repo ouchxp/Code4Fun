cca7e1fe97f28a961687896f5ea054cf2566873c
unknown <ouchxp.me@gmail.com>
2016-03-28 21:51:20 +1300

    Some practise

--- React/es6-react-setup/App.js
+++ React/es6-react-setup/App.js
@@ -5,37 +5,55 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      txt: 'this is the state text'
+      red: 0,
+      green: 0,
+      blue: 0
     };
     this.update = this.update.bind(this)
   }
 
   update(e) {
-    this.setState({ txt: e.target.value })
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+    })
   }
 
   render() {
     return (
       <div>
-        <Widget txt={this.state.txt} update={this.update} />
+        <Slider ref="red" update={this.update}/> {this.state.red}
+        <Slider ref="green" update={this.update}/> {this.state.green}
+        <Slider ref="blue" update={this.update}/> {this.state.blue}
       </div>
     );
   }
 }
 
-const Widget = (props) => {
-  return (
-    <div>
-      <h1>Hello World {props.txt}</h1>
-      <input type="text" onChange={props.update}/>
-    </div>
-  )
-};
+class Slider extends React.Component {
+  render () {
+    return (
+      <div>
+        <input ref="inp" type="range" min="0" max="255" onChange={this.props.update} />
+      </div>
+    );
+  }
+}
 
-Widget.propTypes = {
-  txt: React.PropTypes.string.isRequired,
-  update: React.PropTypes.func.isRequired
-};
+// const Widget = (props) => {
+//   return (
+//     <div>
+//       <h1>Hello World {props.txt}</h1>
+//       <input type="text" onChange={props.update}/>
+//     </div>
+//   )
+// };
+//
+// Widget.propTypes = {
+//   txt: React.PropTypes.string.isRequired,
+//   update: React.PropTypes.func.isRequired
+// };
 
 // Widget.defaultProps = { // use default props could pass the isRequired check
 //   txt: 'default prop'
