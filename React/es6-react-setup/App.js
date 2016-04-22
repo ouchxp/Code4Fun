6ad86f9196e37a357d69e6e6d6c1f3313df7288f
ouchxp <ouchxp.me@gmail.com>
2016-04-22 17:16:41 +1200

    Finish the course

--- React/es6-react-setup/App.js
+++ React/es6-react-setup/App.js
@@ -2,75 +2,79 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      red: 0,
-      green: 0,
-      blue: 0
-    };
-    this.update = this.update.bind(this)
-  }
+    constructor() {
+        super();
+        this.state = {
+            red: 0,
+            green: 0,
+            blue: 0
+        };
+        this.update = this.update.bind(this)
+    }
 
-  update(e) {
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
-      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
-      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
-    })
-  }
+    update(e) {
+        this.setState({
+            red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+            green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+            blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+        })
+    }
 
-  mountSlider() {
-    ReactDOM.render(<Slider update={this.update}/>, document.getElementById("slider"));
-  }
+    mountSlider() {
+        ReactDOM.render(<Slider update={this.update}/>, document.getElementById("slider"));
+    }
 
-  unmountSlider() {
-    ReactDOM.unmountComponentAtNode(document.getElementById("slider"));
-  }
+    unmountSlider() {
+        ReactDOM.unmountComponentAtNode(document.getElementById("slider"));
+    }
 
-  render() {
-    return (
-      <div>
-        <Slider ref="red" update={this.update}/> {this.state.red}
-        <Slider ref="green" update={this.update}/> {this.state.green}
-        <Slider ref="blue" update={this.update}/> {this.state.blue}
-        <RButton>I <Heart/> React</RButton>
-        <button onClick={this.mountSlider.bind(this)}>Mount a Slider</button>
-        <button onClick={this.unmountSlider.bind(this)}>Unmount a Slider</button>
-        <div id="slider"></div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Slider ref="red" update={this.update}/> {this.state.red}
+                <Slider ref="green" update={this.update}/> {this.state.green}
+                <Slider ref="blue" update={this.update}/> {this.state.blue}
+                <RButton>I <Heart/> React</RButton>
+                <button onClick={this.mountSlider.bind(this)}>Mount a Slider</button>
+                <button onClick={this.unmountSlider.bind(this)}>Unmount a Slider</button>
+                <div id="slider"></div>
+
+                <ButtonMixed txt="Button"></ButtonMixed>
+                <LabelMixed txt="Label"/>
+            </div>
+        );
+    }
 
 }
 
 class Slider extends React.Component {
-  static propTypes = {
-    update: React.PropTypes.func.isRequired
-  };
-  render () {
-    console.log("rendering");
-    return (
-      <div>
-        <input ref="inp" type="range" min="0" max="255" onChange={this.props.update} />
-      </div>
-    );
-  }
+    static propTypes = {
+        update: React.PropTypes.func.isRequired
+    };
 
-  componentWillMount() {
-    console.log("will mounted");
-  }
+    render() {
+        console.log("rendering");
+        return (
+            <div>
+                <input ref="inp" type="range" min="0" max="255" onChange={this.props.update}/>
+            </div>
+        );
+    }
 
-  componentDidMount() {
-    console.log("mounted");
-  }
+    componentWillMount() {
+        console.log("will mounted");
+    }
+
+    componentDidMount() {
+        console.log("mounted");
+    }
 }
 
 /** Use this.props.children to reference the innerHTML of component */
 class RButton extends React.Component {
-  render () {
-    return <button>{this.props.children}</button>
-  }
+    render() {
+        return <button>{this.props.children}</button>
+    }
 }
 
 const Heart = () => <span className="glyphicon glyphicon-heart"/>;
@@ -95,5 +99,38 @@ const Heart = () => <span className="glyphicon glyphicon-heart"/>;
 
 // Short hand for state less function component
 // const App = () => <h1>Hello World</h1>;
+
+/** Mixins */
+/** Actually just a simple composition */
+const Mixin = InnerComponent => class extends React.Component {
+    constructor() {
+        super();
+        this.update = this.update.bind(this);
+        this.state = {val: 0};
+    }
+
+    update() {
+        this.setState({val: this.state.val + 1})
+    }
+
+    render() {
+        // Set everything as props for Inner component
+        return <InnerComponent
+            update={this.update}
+            {...this.state}
+            {...this.props} />
+    }
+};
+
+const BaseBtn = (props) => <button onClick={props.update}>{props.txt} - {props.val}</button>
+const BaseLabel = (props) => <label onMouseMove={props.update}>{props.txt} - {props.val}</label>
+const ButtonMixed = Mixin(BaseBtn);
+const LabelMixed = Mixin(BaseLabel);
+
+
+
+
+
+
 
 export default App
