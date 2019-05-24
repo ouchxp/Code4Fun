2be4253f946d34c5444d2070ac88d1d87ae0acc0
Nan <ouchxp.me@gmail.com>
2019-05-24 16:32:24 +1200

    Add some more typeclass example

---
+++ Cats/cats/src/main/scala/chapter1/Typeclass.scala
@@ -0,0 +1,35 @@
+package chapter1
+import me.ouchxp.tool.PrintExpr._
+
+object Typeclass extends App {
+
+  // Define a very simple JSON AST
+  sealed trait Json
+  final case class JsObject(get: Map[String, Json]) extends Json
+  final case class JsString(get: String) extends Json
+  final case class JsNumber(get: Double) extends Json
+  case object JsNull extends Json
+  final case class Person(name: String, email: String)
+
+  // The "serialize to JSON" behaviour is encoded in this trait
+  trait JsonWriter[A] {
+    def write(value: A): Json
+  }
+  implicit val stringWriter: JsonWriter[String] = (value: String) => JsString(value)
+  implicit val personWriter: JsonWriter[Person] = (value: Person) => JsObject(Map(
+    "name" -> JsString(value.name),
+    "email" -> JsString(value.email)
+  ))
+
+
+  def toJson[A](value: A)(implicit w: JsonWriter[A]): Json = w.write(value)
+
+  // -------------------
+  println(toJson(Person("foo", "foo@bar.com")))
+  println(toJson("123"))
+
+  printExpr(toJson(Person("foo", "foo@bar.com")))
+
+
+
+}
