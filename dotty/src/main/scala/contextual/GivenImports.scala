7d83176f7ea4e60466c5f7b2c4efed48c6c7df88
Nan <ouchxp.me@gmail.com>
2019-08-01 12:50:05 +1200

    some more given imports

--- dotty/src/main/scala/contextual/GivenImports.scala
+++ dotty/src/main/scala/contextual/GivenImports.scala
@@ -1,9 +1,20 @@
+import scala.math.Ordering
+import scala.concurrent.ExecutionContext
+case class Monoid[T]()
 case class TC(x: Int)
 
 object A {
-  given tc as TC = TC(3)
+  given /*tc*/ as TC = TC(3)
   def f() given TC = println(the[TC])
 }
+
+object Instances {
+  given intOrd as Ordering[Int] = null;
+  given [T: Ordering] as Ordering[List[T]] = null;
+  given ec as ExecutionContext = null
+  given im as Monoid[Int]
+}
+
 object GivenImports extends App {
   {
     // import only import non-implied members
@@ -20,5 +31,18 @@ object GivenImports extends App {
     // does not compile
     // f()
     // f1()
+  }
+  {
+    // Anonymous given instance can be imported using this syntax
+    import given A.{_:TC}
+    def f1() given (tc: TC) = println(tc)
+    f1()
+  }
+  {
+    // Importing givens of several types T1,...,Tn is expressed by bounding with a union type.
+    // import given A.{_: T1 | ... | Tn}
+    import given Instances.{_: Ordering[?] | ExecutionContext}
+    // or
+    import given Instances.{im, _: Ordering[?]}
   }
 }
