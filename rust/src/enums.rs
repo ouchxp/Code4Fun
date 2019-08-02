90fea728322c3513cd07a89fa44d217586e06058
Nan Wu <ouchxp.me@gmail.com>
2019-08-02 23:46:28 +1200

    add enums

--- rust/src/enums.rs
+++ rust/src/enums.rs
@@ -19,4 +19,15 @@ pub fn test() {
     let x: Option<i32> = Option::None;
     let x_plus_one = x.map(|x| x + 1);
     println!("{:?}", x_plus_one);
+
+    // if let is a shorthand for match one expression and do one thing
+    let some_value = Some(3);
+    if let Some(3) = some_value {
+        println!("three");
+    }
+    if let Some(5) = some_value {
+        println!("five");
+    } else {
+        println!("not five");
+    }
 }
