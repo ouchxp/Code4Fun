e3351ddcd4f6267945c83c6a58725d41e1ec9e26
Nan Wu <ouchxp.me@gmail.com>
2019-08-03 14:51:27 +1200

    update modules

--- rust/src/module.rs
+++ rust/src/module.rs
@@ -10,10 +10,38 @@ mod a {
             println!("{}", "mod C");
         }
     }
+
+    pub mod d {
+        pub fn test() {
+            println!("{}", "mod D, relative path to C");
+            super::c::test();
+        }
+    }
+
+    pub mod e {
+        // e::test is re-exported from C
+        pub use super::c::test;
+    }
 }
 
 pub fn test() {
     a::b::test();
     a::c::test();
+    a::d::test();
     // if module not pub, it cannot be accessed from outside
+
+    {
+        // use can be anywhere and only works in scope
+        use a::b::test;
+        test();
+    }
+
+    {
+        // use with name alias
+        use a::b::test as test_b;
+        test_b();
+    }
+
+    // e::test is re-exported from C
+    a::e::test()
 }
