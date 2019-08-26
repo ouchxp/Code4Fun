bbff27bb35b4fce1156e060020f1cf29d89afb2a
Nan Wu <ouchxp.me@gmail.com>
2019-08-26 23:46:54 +1200

    deref coercion

--- rust/src/box_type.rs
+++ rust/src/box_type.rs
@@ -12,5 +12,12 @@ pub fn test() {
         use List::{Cons, Nil};
         let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
         println!("{:?}", list);
+
+        // Deref Coercion
+        fn f(x: &i32) {
+            println!("{:?}", x);
+        }
+        let b = Box::new(5);
+        f(&b);
     }
 }
