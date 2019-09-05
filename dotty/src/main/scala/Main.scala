7b9991d65c419c5c4381158a8d756cfd5a8ccc31
Nan <ouchxp.me@gmail.com>
2019-09-05 15:15:37 +1200

    Update to dotty 0.18

--- dotty/src/main/scala/Main.scala
+++ dotty/src/main/scala/Main.scala
@@ -2,7 +2,8 @@ def topLevelFunc(x: String) = {
   println(x)
 }
 
-object Main extends App {
+// @main annotation makes a function main
+@main def someFunction() = {
   def msg = "I was compiled by dotty :)"
   println("Hello world!")
   topLevelFunc("Hello World from Top level")
