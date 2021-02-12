c461d420de2b971088a33eb850b5d463f46fdad4
Nan Wu <ouchxp.me@gmail.com>
2021-02-12 13:56:02 +1300

    Update sbt, tiny change to rust cli

--- rust/src/cli.rs
+++ rust/src/cli.rs
@@ -1,8 +1,10 @@
 use std::env;
+use core::panicking::panic_fmt;
 
 pub fn run() {
     let args: Vec<String> = env::args().collect();
-    let command = args[1].clone();
+    let command = args[0].clone();
     // cargo run hello
     println!("command: {}", command);
+    println!("concat: {}", args.join(","))
 }
